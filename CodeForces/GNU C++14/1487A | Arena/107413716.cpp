3604c278a11d8cbf22ecba42a8789758ca81d8ef
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:48:16 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1487A | Arena/107413716.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+int main()
+{ 
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll n;
+  cin>>n;
+ 
+  ll m=INT_MAX;
+  ll a[n];
+  for(ll i=0;i<n;i++){
+   cin>>a[i];
+   m=min(m,a[i]);
+  }
+ 
+  ll c=0;
+  for(ll i=0;i<n;i++){
+   if(a[i]==m) c++;
+  }
+ 
+  cout<<n-c<<endl;
+ }
+ 
+ return 0;
+}
