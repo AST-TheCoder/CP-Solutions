fc46660675598f95c789eea5feef5d89741159ff
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:12:00 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1735B | Tea with Tangerines/174405389.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using min_heap=priority_queue<T, vector<T>, greater<T>>;
+ 
+int main()
+{   
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll n,ans=0;
+  cin>>n;
+ 
+  ll a[n];
+  for(ll i=0;i<n;i++) cin>>a[i];
+ 
+  for(ll i=1;i<n;i++){
+   ans+=(a[i]-1)/(2*a[0]-1);
+  }
+ 
+  cout<<ans<<endl;
+ }
+ 
+ 
+ return 0;
+}
