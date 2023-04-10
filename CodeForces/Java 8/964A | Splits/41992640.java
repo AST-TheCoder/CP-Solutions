55703d7c236a359b64bc04e778458856c86dc96e
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 03:01:27 +0600

    Committing files

---
+++ CodeForces/Java 8/964A | Splits/41992640.java
@@ -0,0 +1,27 @@
+/******************************************************/
+/******************************************************/
+/**            BISMILLAHIR RAHMANIR RAHIM            **/
+/**         Al-Shahriar Tonmoy - Red_Sparkle         **/
+/**  Department of Computer Science and Engineering  **/
+/**               Barisal University                 **/
+/**               It's time to bloom.                **/
+/******************************************************/
+/******************************************************/
+ 
+//package testprograms;
+ 
+import java.util.Scanner;
+ 
+public class TestProgram1 {
+    public static void main(String[] args) {
+        
+        Scanner in=new Scanner(System.in);
+        
+        int n=in.nextInt();
+        
+        System.out.println(n/2+1);
+        
+    }
+}
+ 
+/*****************  ALHAMDULILLAH  *****************/
