c2019aadb5e5e25769196abed554524e058f505a
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:25:59 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1610A | Anti Light's Cell Guessing/136661155.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 100000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main()
+{
+ 
+    ll t;
+    cin>>t;
+ 
+    while(t--){
+        ll n,m;
+        cin>>n>>m;
+        if(n==1 && m==1) cout<<0<<endl;
+ 
+        else cout<<min(min(n,m),2LL)<<endl;
+    }
+    
+ 
+    return 0;
+}
