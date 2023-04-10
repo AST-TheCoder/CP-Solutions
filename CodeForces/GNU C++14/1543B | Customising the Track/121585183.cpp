69467eed68343944aa49c1711c1c701f7379fc2e
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:37:47 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1543B | Customising the Track/121585183.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 100000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main()
+{
+ 
+    ll t;
+    cin>>t;
+ 
+    while(t--){
+        ll n;
+        cin>>n;
+ 
+        ll s=0;
+        for(ll i=0;i<n;i++){
+            ll p;
+            cin>>p;
+ 
+            s+=p;
+        }
+        //cout<<s<<endl;
+        ll x=s%n;
+        ll y=n-x;
+        cout<<x*y<<endl;
+    }
+ 
+    return 0;
+}
