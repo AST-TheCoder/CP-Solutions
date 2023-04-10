afc04fd26796994265233ac59ff837cf4dec82e3
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:58:13 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1393A | Rainbow Dash, Fluttershy and Chess Coloring/89220896.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+ 
+int main()
+{
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll n;
+  cin>>n;
+  cout<<n/2+1<<endl;
+ }
+ 
+ return 0;
+}
