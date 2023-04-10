141a7ce139c5b830d65f8decfc159b69275234d1
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:23:38 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1618A | Polycarp and Sums of Subsequences/139229226.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 100000000000000
+#define min_heap priority_queue <pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+int main()
+{   
+ 
+    ll t;
+    cin>>t;
+ 
+    while(t--){
+        ll a[7];
+        for(ll i=0;i<7;i++) cin>>a[i];
+        cout<<a[0]<<" "<<a[1]<<" "<<a[6]-a[0]-a[1]<<endl;
+    }
+ 
+    return 0;
+}
