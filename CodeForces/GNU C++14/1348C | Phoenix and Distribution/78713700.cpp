0d94a5b659946a24c04a66b22388d19a10a1a3e3
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 02:11:38 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1348C | Phoenix and Distribution/78713700.cpp
@@ -0,0 +1,91 @@
+//Bismillahir Rahmanir Rahim//
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define     fast            ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     in1(X)          scanf("%lli",&(X))
+#define     ins(S)          scanf("%s",(S))
+#define     out(X)          printf("%lli",(X))
+#define     outs(S)         printf("%s",(S))
+#define     printnl         printf("\n")
+#define     End             return 0
+#define     ll              long long int
+#define     Pair            pair<ll,ll>
+#define     F               first
+#define     S               second
+#define     all(v)          v.begin(),v.end()
+#define     mem(a,v)        memset(a,v,sizeof(a))
+#define     Max             1000000000000000014
+#define     Min             -1000000000000000014
+#define     mod             998244353
+#define     range           200007
+#define     pb              push_back
+#define     Vector          vector<ll>
+#define     mp              make_pair
+#define     PI              3.1415926535897
+#define     min_heap        priority_queue <ll, vector<ll>, greater<ll> >
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;//*(x.find_by_order(i)),x.order_of_key(k)
+ 
+/*************** #Template FUNCTIONS# **************/
+ 
+ll pc;void PrintCase(){printf("Case %lli:\n",++pc);}
+ll bigmod(ll n,ll p){if(p==0) return 1;if(p==1)return (n)%mod;if(p%2)return (bigmod(n,p-1)*n)%mod;else{ll x=bigmod(n,p/2);return (x*x)%mod;}}
+ll power(ll n,ll p){if(p==0) return 1;if(p==1)return n;if(p%2)return power(n,p-1)*n;else{ll x=power(n,p/2);return x*x;}}
+ll modinverse(ll n){return bigmod(n,mod-2)%mod;}
+ll Check(ll n,ll i){return (n&(1LL<<i));}
+ll Set(ll n,ll i){return (n|(1LL<<i));}
+ll status[(ll)range/64+5];Vector primes;void sieve(){for(ll i=3;i<=sqrt(range);i+=2){if(Check(status[i/64],i%64)==0){for(ll j=i*i;j<range;j+=2*i){status[j/64]=Set(status[j/64],j%64);}}}primes.pb(2);for(ll i=3;i<range;i+=2)if(Check(status[i/64],i%64)==0)primes.pb(i);}
+double start_time,finish_time;void cnt_time(){start_time=clock();}void print_time(){finish_time=(clock()-start_time)/double(CLOCKS_PER_SEC)*1000;printf("Execution Time: %.2f ms\n",finish_time);} 
+ 
+ll dx[] = {-2, -1, 1, 2,  2,  1, -1, -2};
+ll dy[] = { 1,  2, 2, 1, -1, -2, -2, -1};
+ 
+ll X[]= {0,0,1,-1,1,1,-1,-1};
+ll Y[]= {1,-1,0,0,1,-1,1,-1};
+ 
+/********************** START **********************/
+ 
+int main()
+{
+ 
+ ll t;
+ cin>>t;
+ read:
+ while(t--){
+  ll n,k;
+  cin>>n>>k;
+  string s;
+  cin>>s;
+  sort(all(s));
+  if(s[0]==s[n-1]){
+   ll v=n/k+(n%k!=0);
+   for(ll i=0;i<v;i++){
+    cout<<s[i];
+   }
+   cout<<endl;
+  }
+  else if(s[0]!=s[k-1]){
+   cout<<s[k-1]<<endl;
+  }
+  else if(n!=k && s[k]!=s[k-1] && s[k]==s[n-1]){
+   cout<<s[0];
+   ll x=n-k;
+   ll v=x/k+(x%k!=0);
+   for(ll i=k;i<k+v;i++){
+    cout<<s[k];
+   }cout<<endl;
+  }
+  else{
+   cout<<s[0];
+   for(ll i=k;i<n;i++) cout<<s[i];
+   cout<<endl;
+  }
+ }
+ 
+    End;
+}
+ 
+/*****************  ALHAMDULILLAH  *****************/
