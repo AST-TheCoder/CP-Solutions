f6a82ac14b526ec068a248d58e0a27563a3423dd
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 02:08:04 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1364E | X-OR/83829112.cpp
@@ -0,0 +1,124 @@
+//Bismillahir Rahmanir Rahim//
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define     fast            ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     in1(X)          scanf("%lli",&(X))
+#define     ins(S)          scanf("%s",(S))
+#define     out(X)          printf("%lli",(X))
+#define     outs(S)         printf("%s",(S))
+#define     printnl         printf("\n")
+#define     End             return 0
+#define     ll              long long int
+#define     Pair            pair<ll,ll>
+#define     F               first
+#define     S               second
+#define     all(v)          v.begin(),v.end()
+#define     mem(a,v)        memset(a,v,sizeof(a))
+#define     Max             1000000000000000014
+#define     Min             -1000000000000000014
+#define     mod             998244353
+#define     range           100007
+#define     pb              push_back
+#define     Vector          vector<ll>
+#define     mp              make_pair
+#define     PI              3.1415926535897
+#define     min_heap        priority_queue <ll, vector<ll>, greater<ll> >
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;//*(x.find_by_order(i)),x.order_of_key(k)
+ 
+/*************** #Template FUNCTIONS# **************/
+ 
+ll pc;void PrintCase(){printf("Case %lli:\n",++pc);}
+ll bigmod(ll n,ll p){if(p==0) return 1;if(p==1)return (n)%mod;if(p%2)return (bigmod(n,p-1)*n)%mod;else{ll x=bigmod(n,p/2);return (x*x)%mod;}}
+ll power(ll n,ll p){if(p==0) return 1;if(p==1)return n;if(p%2)return power(n,p-1)*n;else{ll x=power(n,p/2);return x*x;}}
+ll modinverse(ll n){return bigmod(n,mod-2)%mod;}
+ll Check(ll n,ll i){return (n&(1LL<<i));}
+ll Set(ll n,ll i){return (n|(1LL<<i));}
+ll status[(ll)range/64+5];Vector primes;void sieve(){for(ll i=3;i<=sqrt(range);i+=2){if(Check(status[i/64],i%64)==0){for(ll j=i*i;j<range;j+=2*i){status[j/64]=Set(status[j/64],j%64);}}}primes.pb(2);for(ll i=3;i<range;i+=2)if(Check(status[i/64],i%64)==0)primes.pb(i);}
+double start_time,finish_time;void cnt_time(){start_time=clock();}void print_time(){finish_time=(clock()-start_time)/double(CLOCKS_PER_SEC)*1000;printf("Execution Time: %.2f ms\n",finish_time);} 
+ 
+ll dx[] = {-2, -1, 1, 2,  2,  1, -1, -2};
+ll dy[] = { 1,  2, 2, 1, -1, -2, -2, -1};
+ 
+ll X[]= {0,0,1,-1,1,1,-1,-1};
+ll Y[]= {1,-1,0,0,1,-1,1,-1};
+ 
+/********************** START **********************/
+ 
+int main()
+{
+    
+    cout.flush();
+ 
+    ll n;
+    cin>>n;
+ 
+    ll c[12],ok=1,v=0,pot=1,z_id=1;
+    Vector ans;
+    mem(c,-1);
+ 
+    while(ok){
+        ll i=rand()%n+1;ll j=rand()%n+1;
+        if(i==j) continue;
+        ok=0;
+        ll x;
+        cout<<"? "<<i<<" "<<j<<endl;
+        cin>>x;
+        if(i==1) i=j;
+        for(ll idx=0;idx<=11;idx++){
+            if(!(x%2)) c[idx]=i;
+            x/=2;
+            if(c[idx]==-1) ok=1;
+        }
+    }
+ 
+    for(ll i=0;i<=11;i++){
+        ll x;
+        cout<<"? "<<1<<" "<<c[i]<<endl;
+        cin>>x;
+ 
+        if(x&(1<<i)) v+=pot;
+        pot*=2;
+    }
+    for(ll i=2,idx=1;i<=n && v;i++){
+        ll x;
+        cout<<"? "<<idx<<" "<<i<<endl;
+        cin>>x;
+        if((x&v)==x){
+            v=0,pot=1,idx=i;
+            for(ll j=0;j<=11;j++){
+                if(i==c[j]) continue;
+                cout<<"? "<<i<<" "<<c[j]<<endl;
+                cin>>x;
+ 
+                if(x&(1<<j)) v+=pot;
+                pot*=2;
+            }
+            if(!v){
+                z_id=idx;
+                break;
+            }
+        }
+    }
+ 
+    for(ll i=1;i<=n;i++){
+        if(i==z_id) ans.pb(0LL);
+        else{
+            ll x;
+            cout<<"? "<<i<<" "<<z_id<<endl;
+            cin>>x;
+            ans.pb(x);
+        }
+    }
+ 
+    cout<<"!";
+    for(ll i=0;i<n;i++) cout<<" "<<ans[i];
+    cout<<endl;
+ 
+    End;
+}
+ 
+/*****************  ALHAMDULILLAH  *****************/
