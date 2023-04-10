425beb367f37dceef99d7609f42eda1724a271af
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:53:41 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1409B | Minimum Product/91958502.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main()
+{
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll a,b,x,y,n;
+  cin>>a>>b>>x>>y>>n;
+ 
+  ll d1=a-x,d2=b-y;
+ 
+  ll p=(a-min(d1,n))*(b-min(d2,max(n-d1,0LL)));
+  ll q=(a-min(d1,max(n-d2,0LL)))*(b-min(d2,n));
+ 
+  cout<<min(p,q)<<endl;
+ }
+ 
+ return 0;
+}
