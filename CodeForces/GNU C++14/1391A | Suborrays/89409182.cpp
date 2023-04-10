931d4dd69c41daa577d7838ec735ade31407a28a
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:57:49 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1391A | Suborrays/89409182.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+ 
+int main()
+{
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll n;
+  cin>>n;
+  for(ll i=n;i>=1;i--) cout<<i<<" ";
+  cout<<endl;
+ }
+ 
+ return 0;
+}
