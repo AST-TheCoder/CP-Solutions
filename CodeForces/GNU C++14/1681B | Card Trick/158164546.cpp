e1144e380b4c246d23e070d7f8e58c02ee02675a
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:15:39 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1681B | Card Trick/158164546.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main()
+{   
+    ll t,ii=0;
+    cin>>t;
+ 
+    read:
+    while(t--){
+        
+        ll n;
+        cin>>n;
+        ll a[n+1];
+        for(ll i=1;i<=n;i++) cin>>a[i];
+ 
+        ll ans=1;
+        ll m;
+        cin>>m;
+ 
+        for(ll i=1;i<=m;i++){
+            ll x;
+            cin>>x;
+ 
+            ans+=x;
+            ans%=n;
+            if(!ans) ans=n;
+        }
+ 
+        cout<<a[ans]<<endl;
+    }
+ 
+    return 0;
+}
