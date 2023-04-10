3b451b46df7d53908dd10f817e5f7033c63e72ae
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:17:28 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1673B | A Perfectly Balanced String?/155412106.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main()
+{   
+    
+    ll t,ii=0;
+    cin>>t;
+ 
+    read:
+    while(t--){
+ 
+        string s;
+        cin>>s;
+ 
+        set<char> x;
+        for(ll i=0;i<s.size();i++) x.insert(s[i]);
+ 
+        for(ll i=1;i<s.size();i++){
+            if(i<x.size()){
+                if(s[i]==s[i-1]){
+                    cout<<"NO"<<endl;
+                    goto read;
+                }
+            }
+            else{
+                if(s[i]!=s[i-x.size()]){
+                    cout<<"NO"<<endl;
+                    goto read;
+                }
+            }
+        }
+        cout<<"YES"<<endl;
+    }
+ 
+    return 0;
+}
