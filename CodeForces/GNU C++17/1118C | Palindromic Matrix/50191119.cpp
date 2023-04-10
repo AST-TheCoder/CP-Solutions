6bc829c42a6fdbac4bd476118d693f868d02264d
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 02:39:53 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1118C | Palindromic Matrix/50191119.cpp
@@ -0,0 +1,172 @@
+/******************************************************/
+/******************************************************/
+/**            BISMILLAHIR RAHMANIR RAHIM            **/
+/**         Al-Shahriar Tonmoy - Red_Sparkle         **/
+/**  Department of Computer Science and Engineering  **/
+/**               Barisal University                 **/
+/**               It's time to bloom.                **/
+/******************************************************/
+/******************************************************/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define     fRead           freopen("input.txt","r",stdin)
+#define     fWrite          freopen("output.txt","w",stdout)
+#define     fast            ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     in1(X)          scanf("%lli",&(X))
+#define     in2(X,Y)        scanf("%lli%lli",&(X),&(Y))
+#define     in3(X,Y,Z)      scanf("%lli%lli%lli",&(X),&(Y),&(Z))
+#define     ins(S)          scanf("%s",(S))
+#define     out(X)          printf("%lli",(X))
+#define     outs(S)         printf("%s",(S))
+#define     printnl         printf("\n")
+#define     printsp         printf(" ");
+#define     End             return 0
+#define     ll              long long int
+#define     ul              unsigned long long int
+#define     dob             double
+#define     st              string
+#define     pairl           pair<ll,ll>
+#define     max3(a,b,c)     max(a,max(b,c))
+#define     min3(a,b,c)     min(a,min(b,c))
+#define     fr              first
+#define     se              second
+#define     T               true
+#define     F               false
+#define     nl              "\n"
+#define     all(v)          v.begin(),v.end()
+#define     mem(a,v)        memset(a,v,sizeof(a))
+#define     maxi            1000000000000000014
+#define     mini            -100000000000014
+#define     mod             998244353
+#define     an              300007
+#define     mapi            map<ll,ll>
+#define     pi              push_back
+#define     po              pop_back
+#define     ve              vector<ll>
+#define     mp              make_pair
+#define     PI              3.14159265
+ 
+/*************** #Template FUNCTIONS# **************/
+ 
+/// ll pc;void PrintCase(){printf("Case %lli: ",++pc);}
+/// ll GCD(ll a,ll b){if(a<b)swap(a,b);if(b==1) return 1;if(!b) return a;if(!(a%b)) return b;else return GCD(b,a%b);}
+/// ll LCM(ll a,ll b){return (a*b)/GCD(a,b);}
+/// ll bigmod(ll n,ll p){if(p==0) return 1;if(p==1)return (n+mod)%mod;if(p%2)return (bigmod(n,p-1)*n+mod)%mod;else{ll x=bigmod(n,p/2);return (x*x+mod)%mod;}}
+/// ll modinverse(ll n){return bigmod(n,mod-2)%mod;}
+/// bool sc[an];ve pr;void sieve(){for(ll i=2;i*i<an;i++){if(!sc[i]){for(ll j=i*i;j<an;j+=i) sc[j]=1;}}pr.pi(2);for(ll i=3;i<an;i+=2) if(!sc[i]) pr.pi(i);}
+/// pairl pf[an];void pri_fact(ll n){ll i,u=0;for(i=0;i<pr.size() && n!=1;i++){ll c=0;while(!(n%pr[i])){n/=pr[i];c++;} if(c){pf[u].fr=pr[i];pf[u].se=c;u++;}}if(n>1){pf[u].fr=n;pf[u].se=1;}}
+/// ve de;void devisors(ll n){ll i;for(i=1;i<=sqrt(n);i++){if(!(n%i) && i!=sqrt(n)){de.pi(i);de.pi(n%i);}}if((i-1)*(i-1)==n) de.pi(i-1);sort(all(de));}
+ 
+/********************** START **********************/
+ll x[1001][1001],c[1001],f;
+int main()
+{
+ 
+    fast;
+ 
+    ll n,j,k=1,i,p=1,q=1;
+    cin>>n;
+ 
+    ll a[n*n];
+    for(i=0;i<n*n;i++){
+        cin>>a[i];
+        c[a[i]]++;
+ 
+    }
+    //cout<<i;
+ 
+    for(i=1;i<=1000;i++){
+        //cout<<i<<" "<<c[i];
+        if(n%2){
+            if(c[i]%4 && c[i]%2 && !f){
+                f=i;
+                x[n/2+1][n/2+1]=f;
+                c[i]--;
+                //continue;
+            }
+ 
+            if(c[i]%2==0 && c[i]%4){
+                if(p<n/2+1){
+                    x[n/2+1-p][n/2+1]=i;
+                    x[n/2+1+p][n/2+1]=i;
+                    p++;
+                    c[i]-=2;
+                    continue;
+                }
+                if(q<n/2+1){
+                    x[n/2+1][n/2+1+q]=i;
+                    x[n/2+1][n/2+1-q]=i;
+                    q++;
+                    c[i]-=2;
+                    continue;
+                }
+            }
+ 
+            if(c[i]%4){
+                cout<<"NO"<<endl;
+                return 0;
+            }
+        }
+        else{
+            if(c[i]%4){
+                cout<<"NO"<<endl;
+                return 0;
+            }
+        }
+    }
+ 
+    cout<<"YES"<<endl;
+ 
+    if(n%2){
+        while(p<n/2+1){
+            for(;k<=1000;k++){
+                if(c[k]){
+                    x[n/2+1-p][n/2+1]=k;
+                    x[n/2+1+p][n/2+1]=k;
+                    p++;
+                    c[k]-=2;
+                    break;
+                }
+            }
+        }
+        while(q<n/2+1){
+            for(;k<=1000;k++){
+                if(c[k]){
+                    x[n/2+1][n/2+1+q]=k;
+                    x[n/2+1][n/2+1-q]=k;
+                    q++;
+                    c[k]-=2;
+                    break;
+                }
+            }
+        }
+    }
+ 
+ 
+    for(i=1;i<=n/2;i++){
+        for(j=1;j<=n/2;j++){
+            for(;k<=1000;k++){
+                if(c[k]){
+                    c[k]-=4;
+                    x[i][j]=k;
+                    x[n-i+1][j]=k;
+                    x[i][n-j+1]=k;
+                    x[n-i+1][n-j+1]=k;
+                    break;
+                }
+            }
+        }
+    }
+ 
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            cout<<x[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+ 
+ 
+    End;
+}
+/*****************  ALHAMDULILLAH  *****************/
