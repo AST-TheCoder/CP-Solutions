d85150ca38e6158b16cd4e9930867a4eaae07ece
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 03:08:48 +0600

    Committing files

---
+++ CodeForces/GNU C++/1003B | Binary String Constructing/39931763.cpp
@@ -0,0 +1,128 @@
+/******************************************************/
+/******************************************************/
+/**            BISMILLAHIR RAHMANIR RAHIM            **/
+/**         Al-Shahriar Tonmoy - Red_Sparkle         **/
+/**  Department of Computer Science and Engineering  **/
+/**               Barisal University                 **/
+/**               It's time to bloom.                **/
+/******************************************************/
+/******************************************************/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll              long long int
+#define     ul              unsigned long long int
+#define     dob             double
+#define     st              string
+#define     ch              char
+#define     pairl           pair<ll,ll>
+#define     pairs           pair<string,int>
+#define     pairi           pair<int,string>
+#define     pairss          pair<string,string>
+#define     max3(a,b,c)     max(a,max(b,c))
+#define     max4(a,b,c,d)   max(max3(a,b,c),d)
+#define     min3(a,b,c)     min(a,min(b,c))
+#define     min4(a,b,c,d)   min(a,min3(b,c,d))
+#define     fr              first
+#define     se              second
+#define     T               true
+#define     F               false
+#define     sqr(x)          pow(x,2)
+#define     all(v)          v.begin(),v.end()
+#define     fast            ios_base::sync_with_stdio(0);cin.tie(0)
+#define     mem(a,v)        memset(a,v,sizeof(a))
+#define     maxi            1000000000000014
+#define     mini            -100000000000014
+#define     mod             1000000007
+#define     an              1000010
+#define     maps            map<string,int>
+#define     mapi            map<int,int>
+#define     pi              push_back
+#define     po              pop_back
+#define     ve              vector<ll>
+#define     mp              make_pair
+ 
+ll a[500],m;
+ 
+int main()
+{
+ 
+    ll n,i,x;
+ 
+    cin>>n>>m>>x;
+ 
+    if(m>n){
+ 
+        for(i=1;i<x;i++){
+ 
+            if(i%2){
+ 
+                cout<<1;
+                m--;
+ 
+            }
+ 
+            else{
+ 
+                cout<<0;
+                n--;
+ 
+            }
+ 
+        }
+ 
+        if(!(x%2)){
+ 
+            for(i=1;i<=n;i++) cout<<0;
+            for(i=1;i<=m;i++) cout<<1;
+ 
+        }
+ 
+        else{
+ 
+            for(i=1;i<=m;i++) cout<<1;
+            for(i=1;i<=n;i++) cout<<0;
+ 
+        }
+ 
+    }
+ 
+    else{
+ 
+        for(i=1;i<x;i++){
+ 
+            if(i%2){
+ 
+                cout<<0;
+                n--;
+ 
+            }
+ 
+            else{
+ 
+                cout<<1;
+                m--;
+ 
+            }
+ 
+        }
+ 
+        if(x%2){
+ 
+            for(i=1;i<=n;i++) cout<<0;
+            for(i=1;i<=m;i++) cout<<1;
+ 
+        }
+ 
+        else{
+ 
+            for(i=1;i<=m;i++) cout<<1;
+            for(i=1;i<=n;i++) cout<<0;
+ 
+        }
+ 
+    }
+ 
+ return 0;
+}
+/*****************  ALHAMDULILLAH  *****************/
