214143facb185aa3ec54817f321fe2d32adfeaea
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:31:39 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1562C | Rings/127082990.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 100000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main()
+{   
+    
+    ll t;
+    cin>>t;
+ 
+    read:
+    while(t--){
+        ll n;
+        string s;
+        cin>>n>>s;
+ 
+        ll c1=count(all(s),'1');
+        ll c0=count(all(s),'0');
+ 
+        if(c1==n || c0==n){
+            cout<<1<<" "<<n-1<<" "<<2<<" "<<n<<endl;
+            continue;
+        }
+        ll h=n/2;
+        for(ll i=0;i<n;i++){
+            if(s[i]=='0'){
+                i++;
+                if(i<=h) cout<<i<<" "<<n<<" "<<i+1<<" "<<n<<endl;
+                else cout<<1<<" "<<i<<" "<<1<<" "<<i-1<<endl;
+                break;
+            }
+        }
+    }
+ 
+    return 0;
+}
