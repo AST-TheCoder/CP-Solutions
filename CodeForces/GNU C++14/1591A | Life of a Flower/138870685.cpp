3de29eb4fae6dd76f309a9c97e9b47074cbadbc6
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:23:58 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1591A | Life of a Flower/138870685.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 100000000000000
+#define min_heap priority_queue <pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+int main()
+{   
+ 
+    ll t;
+    cin>>t;
+ 
+    read:
+    while(t--){
+        ll n,ans=1;
+        cin>>n;
+ 
+        ll a[n];
+        for(ll i=0;i<n;i++){
+            cin>>a[i];
+        }
+ 
+        for(ll i=0;i<n;i++){
+            if(a[i]){
+                if(i && a[i-1]) ans+=4;
+                ans++;
+            }
+            else{
+                if(i && !a[i-1]){
+                    cout<<-1<<endl;
+                    goto read;
+                }
+            }
+        }
+        cout<<ans<<endl;
+    }
+ 
+    return 0;
+}
