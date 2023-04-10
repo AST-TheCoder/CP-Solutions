6fad8f63fc7fc32f3d12b8fc39f36e78ca7351c2
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:53:23 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1405A | Permutation Forgery/92205955.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main()
+{
+ 
+ ll t;
+ cin>>t;
+ 
+ read:
+ while(t--){
+  ll n;
+  cin>>n;
+ 
+  ll a[n+1];
+  for(ll i=1;i<=n;i++) cin>>a[i];
+ 
+  for(ll i=n;i>=1;i--) cout<<a[i]<<" ";
+  cout<<endl;
+ }
+ 
+ return 0;
+}
