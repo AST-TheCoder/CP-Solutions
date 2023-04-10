21345abcef6c8556d879f09184127a796a3317a7
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:03:52 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1780B | GCD Partition/190511177.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using min_heap=priority_queue<T, vector<T>, greater<T>>;
+ 
+int main()
+{
+ 
+    ll t;
+    cin>>t;
+ 
+    while(t--){
+        ll n;
+        cin>>n;
+ 
+        ll a[n],x=0,y=0;
+        for(ll i=0;i<n;i++){
+            cin>>a[i];
+            y+=a[i];
+        }
+        ll ans=0;
+        for(ll i=0;i<n-1;i++){
+            y-=a[i];
+            x+=a[i];
+            ans=max(ans,__gcd(x,y));
+        }
+        cout<<ans<<endl;
+    }
+ 
+    return 0;
+}
