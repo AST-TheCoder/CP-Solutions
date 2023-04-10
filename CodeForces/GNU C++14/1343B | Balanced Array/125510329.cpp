3d349df82475931a0b7596adef3d0bfba7721462
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:33:37 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1343B | Balanced Array/125510329.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 100000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll n,k,mod=1000000007;
+ 
+ll bigmod(ll v,ll p){
+    if(!p) return 1;
+    if(p%2) return (v*bigmod(v,p-1))%mod;
+    ll x=bigmod(v,p/2);
+    return (x*x)%mod;
+}
+ 
+ll solve(ll pos){
+    if(pos>k) return 1;
+    ll ans=0,cnt=0;
+    if(n%2==0){
+        cnt=-1;
+        ans=bigmod(2,n*(k-pos));
+    }
+    else cnt=1;
+    ll x=solve(pos+1);
+    cnt+=bigmod(2,n-1);
+    ans+=(x*cnt);
+    return ans%mod;
+}
+ 
+int main()
+{   
+ 
+    ll t;
+    cin>>t;
+ 
+    while(t--){
+        ll n;
+        cin>>n;
+        if(n%4) cout<<"NO"<<endl;
+        else{
+            cout<<"YES"<<endl;
+            for(ll i=1,s=0;i<=n;i++){
+                if(i<=n/2){
+                    cout<<(2*i)<<" ";
+                    s+=(2*i);
+                }
+                else if(i<n){
+                    cout<<(i-n/2)*2-1<<" ";
+                    s-=((i-n/2)*2-1);
+                }
+                else cout<<s<<endl;
+            }
+        }
+    }
+ 
+    return 0;
+}
