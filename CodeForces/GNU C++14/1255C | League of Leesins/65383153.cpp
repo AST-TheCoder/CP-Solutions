982d9c7f0479d7c15623b0898227ea985d0cbde4
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 02:20:09 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1255C | League of Leesins/65383153.cpp
@@ -0,0 +1,125 @@
+//Bismillahir Rahmanir Rahim//
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define     fast            ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     in1(X)          scanf("%lli",&(X))
+#define     ins(S)          scanf("%s",(S))
+#define     out(X)          printf("%lli",(X))
+#define     outs(S)         printf("%s",(S))
+#define     printnl         printf("\n")
+#define     End             return 0
+#define     ll              long long int
+#define     Pair            pair<ll,ll>
+#define     F               first
+#define     S               second
+#define     all(v)          v.begin(),v.end()
+#define     mem(a,v)        memset(a,v,sizeof(a))
+#define     Max             1000000000000000014
+#define     Min             -1000000000000000014
+#define     mod             100000007
+#define     range           100007
+#define     pb              push_back
+#define     Vector          vector<ll>
+#define     mp              make_pair
+#define     PI              3.14159265
+#define     min_heap        priority_queue <ll, vector<ll>, greater<ll> >
+#define     ordered_set     tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>  //*(x.find_by_order(k)),x.order_of_key(k)
+ 
+/*************** #Template FUNCTIONS# **************/
+ 
+ll pc;void PrintCase(){printf("Case %lli: ",++pc);}
+ll bigmod(ll n,ll p){if(p==0) return 1;if(p==1)return (n+mod)%mod;if(p%2)return (bigmod(n,p-1)*n+mod)%mod;else{ll x=bigmod(n,p/2);return (x*x+mod)%mod;}}
+ll power(ll n,ll p){if(p==0) return 1;if(p==1)return n;if(p%2)return power(n,p-1)*n;else{ll x=power(n,p/2);return x*x;}}
+ll modinverse(ll n){return bigmod(n,mod-2)%mod;}
+ll Check(ll n,ll i){return (n&(1LL<<i));}
+ll Set(ll n,ll i){return (n|(1LL<<i));}
+ll status[(ll)range/64+5];Vector primes;void sieve(){for(ll i=3;i<=sqrt(range);i+=2){if(Check(status[i/64],i%64)==0){for(ll j=i*i;j<range;j+=2*i){status[j/64]=Set(status[j/64],j%64);}}}primes.pb(2);for(ll i=3;i<range;i+=2)if(Check(status[i/64],i%64)==0)primes.pb(i);}
+ 
+ll dx[] = {-2, -1, 1, 2,  2,  1, -1, -2};
+ll dy[] = { 1,  2, 2, 1, -1, -2, -2, -1};
+ 
+ll X[]= {0,0,1,-1,1,1,-1,-1};
+ll Y[]= {1,-1,0,0,1,-1,1,-1};
+ 
+/********************** START **********************/
+ 
+int main()
+{
+ 
+    fast;
+    
+    ll n;
+    in1(n);
+ 
+    ll a[n][4],cnt=0,s=0;
+    Vector x[n+5],ans;
+ 
+    for(ll i=0;i<n-2;i++){
+        for(ll j=0;j<3;j++){
+            in1(a[i][j]);
+            x[a[i][j]].pb(i);
+        }
+    }
+ 
+    for(ll i=1;i<=n;i++){
+        if(x[i].size()==1){
+            s=x[i][0];
+            ans.pb(i);
+            break;
+        }
+    }
+ 
+    while(cnt!=n-2){
+        if(cnt==0){
+            for(ll i=0;i<3;i++){
+                if(x[a[s][i]].size()<=2 && ans[0]!=a[s][i]){
+                    ans.pb(a[s][i]);
+                    for(ll j=0;j<2;j++){
+                        if(x[a[s][i]][j]==s) x[a[s][i]].erase(x[a[s][i]].begin()+j);
+                    }
+                }
+            }
+            for(ll i=0;i<3;i++){
+                if(x[a[s][i]].size()==3){
+                    ans.pb(a[s][i]);
+                    for(ll j=0;j<3;j++){
+                        if(x[a[s][i]][j]==s) x[a[s][i]].erase(x[a[s][i]].begin()+j);
+                    }
+                    break;
+                }
+            }
+            cnt++;
+            continue;
+        }
+        for(ll i=0,ok=0;i<x[ans[cnt]].size();i++){
+            for(ll j=0;j<x[ans[cnt+1]].size();j++){
+                if(x[ans[cnt]][i]==x[ans[cnt+1]][j]){
+                    ok=1;
+                    s=x[ans[cnt]][i];
+                    x[ans[cnt]].erase(x[ans[cnt]].begin()+i);
+                    x[ans[cnt+1]].erase(x[ans[cnt+1]].begin()+j);
+                    break;
+                }
+            }
+            if(ok=1) break;
+        }
+        for(ll i=0;i<3;i++){
+            if(a[s][i]!=ans[cnt] && a[s][i]!=ans[cnt+1]){
+                ans.pb(a[s][i]);
+                for(ll j=0;j<x[a[s][i]].size();j++){
+                    if(x[a[s][i]][j]==s) x[a[s][i]].erase(x[a[s][i]].begin()+j);
+                }
+                break;
+            }
+        }
+        cnt++;
+    }
+ 
+    for(ll i=0;i<n;i++) printf("%lli ",ans[i]);
+ 
+    End;
+}
+/*****************  ALHAMDULILLAH  *****************/
