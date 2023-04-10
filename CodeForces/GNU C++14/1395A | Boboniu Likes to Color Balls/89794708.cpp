3d3c21ce5d8c0eb0670af7611b9c086cd698bf0d
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:57:17 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1395A | Boboniu Likes to Color Balls/89794708.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+ 
+int main()
+{
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll r,g,b,w;
+  cin>>r>>g>>b>>w;
+  if(((r%2)+(g%2)+(b%2)+(w%2)<=1) || (r && g && b && (r-1)%2+(g-1)%2+(b-1)%2+(w+3)%2<=1)) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+ }
+ 
+ return 0;
+}
