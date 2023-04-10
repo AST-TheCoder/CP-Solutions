4f98f940d08c43a53d9053275f6a64a07dc7b869
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:14:16 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1708B | Difference of GCDs/164461564.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main()
+{   
+    
+    ll t;
+    cin>>t;
+ 
+    read:
+    while(t--){
+        ll n,l,r;
+        cin>>n>>l>>r;
+ 
+        vector<ll> v;
+        for(ll i=1;i<=n;i++){
+            ll x=r/i;
+            x*=i;
+            if(x<l){
+                cout<<"NO"<<endl;
+                goto read;
+            }
+            v.pb(x);
+        }
+        cout<<"YES"<<endl;
+        for(ll i=0;i<n;i++) cout<<v[i]<<" ";
+        cout<<endl;
+    }
+ 
+    return 0;
+}
