40c683d7d65340b15b66db17b558b8c94b455fa8
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 03:12:39 +0600

    Committing files

---
+++ CodeForces/GNU C++14/890C | Petya and Catacombs/38721424.cpp
@@ -0,0 +1,85 @@
+/******************************************************/
+/******************************************************/
+/**            BISMILLAHIR RAHMANIR RAHIM            **/
+/**         Al-Shahriar Tonmoy - Red_Sparkle         **/
+/**  Department of Computer Science and Engineering  **/
+/**               Barisal University                 **/
+/**               It's time to bloom.                **/
+/******************************************************/
+/******************************************************/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll              long long int
+#define     ul              unsigned long long int
+#define     dob             double
+#define     st              string
+#define     ch              char
+#define     pairl           pair<ll,ll>
+#define     pairs           pair<string,int>
+#define     pairi           pair<int,string>
+#define     pairss          pair<string,string>
+#define     max3(a,b,c)     max(a,max(b,c))
+#define     max4(a,b,c,d)   max(max3(a,b,c),d)
+#define     min3(a,b,c)     min(a,min(b,c))
+#define     min4(a,b,c,d)   min(a,min3(b,c,d))
+#define     fr              first
+#define     T               true
+#define     F               false
+#define     se              second
+#define     sqr(x)          pow(x,2)
+#define     all(v)          v.begin(),v.end()
+#define     maxi            1000000000000014
+#define     mini            -1000000000000014
+#define     mod             1000000007
+#define     an              1000000
+#define     maps            map<string,int>
+#define     mapi            map<int,int>
+#define     pi              push_back
+#define     po              pop_back
+#define     ve              vector<ll>
+#define     mp              make_pair
+#define     nu              200005
+ 
+int a[nu],b[nu],c;
+ 
+int main()
+{
+    int n,i;
+ 
+    cin>>n;
+ 
+    for(i=1;i<=n;i++){
+ 
+        cin>>b[i];
+ 
+        if(!b[i]){
+ 
+            c++;
+ 
+            a[i]=1;
+ 
+        }
+ 
+        else if(a[b[i]]==1){
+ 
+            a[b[i]]=0;
+ 
+            a[i]=1;
+ 
+        }
+ 
+        else{
+ 
+            c++;
+ 
+            a[i]=1;
+        }
+    }
+ 
+    cout<<c;
+ 
+ return 0;
+}
+/*****************  ALHAMDULILLAH  *****************/
+ 
