1e3b98c4451d391c7da58eee54cc98aea17198f1
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:02:36 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1789A | Serval and Mocha's Array/194927043.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using min_heap=priority_queue<T, vector<T>, greater<T>>;
+ 
+int main()
+{
+ 
+    ll t;
+    cin>>t;
+ 
+    while(t--){
+        ll n;
+        cin>>n;
+ 
+        ll a[n];
+        for(ll i=0;i<n;i++) cin>>a[i];
+ 
+        ll gcd=Max;
+        for(ll i=0;i<n;i++){
+            for(ll j=i+1;j<n;j++){
+                gcd=min(gcd,__gcd(a[i],a[j]));
+            }
+        }
+ 
+        if(gcd<=2) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+ 
+    return 0;
+}
