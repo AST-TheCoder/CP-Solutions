bc0e5c2ba94cec421485089c7bb39496753aeb56
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:18:46 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1654A | Maximum Cake Tastiness/150229877.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main()
+{   
+    
+    ll t;
+    cin>>t;
+ 
+    while(t--){
+        ll n;
+        cin>>n;
+ 
+        ll a[n];
+        for(ll i=0;i<n;i++) cin>>a[i];
+        sort(a,a+n,greater<ll>());
+        cout<<a[0]+a[1]<<endl;
+    }
+ 
+    return 0;
+}
