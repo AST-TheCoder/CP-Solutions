8ba8f60adcf15ed6d60834bd212ceec66579d34e
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:15:32 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1686A | Everything Everywhere All But One/158416633.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main()
+{   
+    ll t,ii=0;
+    cin>>t;
+ 
+    read:
+    while(t--){
+        
+        ll n;
+        cin>>n;
+ 
+        ll a[n],s=0;
+        for(ll i=0;i<n;i++){
+            cin>>a[i];
+            s+=a[i];
+        }
+ 
+        for(ll i=0;i<n;i++){
+            if(a[i]*n==s){
+                cout<<"YES"<<endl;
+                goto read;
+            }
+        }
+        cout<<"NO"<<endl;
+    }
+ 
+    return 0;
+}
