aec20bf1115fe21782aae6e4ddb2d6d92af2be55
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 02:01:01 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1372A | Omkar and Completion/86517214.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+ 
+int main()
+{
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll n;
+  cin>>n;
+ 
+  for(ll i=1;i<=n;i++) cout<<1<<" ";
+   cout<<endl;
+ }
+ 
+    return 0;
+}
