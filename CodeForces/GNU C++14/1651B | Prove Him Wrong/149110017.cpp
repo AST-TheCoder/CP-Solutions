96a785733823d59a88b20e6eb0ea37ec8c2aee56
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:19:13 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1651B | Prove Him Wrong/149110017.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mark[2000001],a[1000001],chk[2000001];
+ 
+int main()
+{   
+    
+    ll t;
+    scanf("%lli",&t);
+ 
+    read:
+    while(t--){
+        ll n;
+        cin>>n;
+ 
+        if(n>19) cout<<"NO"<<endl;
+        else{
+            cout<<"YES"<<endl;
+            for(ll i=1,c=1;i<=n;i++,c*=3){
+                cout<<c<<" ";
+            }cout<<endl;
+        }
+    }
+ 
+    return 0;
+}
