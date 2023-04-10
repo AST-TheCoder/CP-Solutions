8bb87e2a9258d9dc9958ba6fc5fee5db952f1c92
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:58:47 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1382D | Unmerge/87693365.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+ 
+int main()
+{
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll n;
+  cin>>n;
+  ll idx[2*n+1],chk=2*n;
+  vector<ll> coin;
+  coin.pb(0LL);
+ 
+  for(ll i=1;i<=2*n;i++){
+   ll x;
+   cin>>x;
+   idx[x]=i;
+  }
+  for(ll i=2*n;i>=1;i--){
+   if(idx[i]<=chk){
+    coin.pb(chk-idx[i]+1);
+    chk=idx[i]-1;
+   }
+  }
+ 
+  ll cost=n,coin_no=coin.size()-1;
+  ll dp[coin_no+1][cost+1];
+  memset(dp,0,sizeof(dp));
+ 
+  for(ll i=0;i<=coin_no;i++) dp[i][0]=1;
+ 
+  for(ll i=1;i<=coin_no;i++){
+   for(ll j=1;j<=cost;j++){
+    dp[i][j]=dp[i-1][j];
+    if(coin[i]<=j) dp[i][j]|=dp[i-1][j-coin[i]];   
+   }
+  }
+ 
+  if(dp[coin_no][cost]) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+ }
+ 
+ return 0;
+}
