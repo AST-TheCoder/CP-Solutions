81936ff3db80508d5a23de9c03c9a495b7032356
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:45:52 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1501B | Napoleon Cake/109848735.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+int main()
+{ 
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll n;
+  cin>>n;
+ 
+  ll a[n+1],m=0,ans[n+1];
+  memset(ans,0,sizeof(ans));
+ 
+  for(ll i=1;i<=n;i++){
+   cin>>a[i];
+  }
+ 
+  for(ll i=n;i>=1;i--){
+   m=max(m,a[i]);
+   if(m){
+    ans[i]=1;
+    m--;
+   }
+  }
+ 
+  for(ll i=1;i<=n;i++){
+   cout<<ans[i]<<" ";
+  }cout<<endl;
+ 
+ }
+ 
+ return 0;
+}
