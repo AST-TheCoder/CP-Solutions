d8dd30e7e6b27b07e6f024a6464217cb810e9e89
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:56:36 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1392E | Omkar and Duck/90148444.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+ 
+int main()
+{
+ 
+ cout.flush();
+ 
+ ll n;
+ cin>>n;
+ ll a[n+3][n+3];
+ 
+ for(ll i=1;i<=n;i++){
+  ll p=pow(2LL,i-1LL);
+  if(!(i%2)) p=0;
+  for(ll j=1;j<=n;j++){
+   cout.flush();
+   cout<<p<<" ";
+   a[i][j]=p;
+   p*=2;
+  }
+  cout.flush();
+  cout<<endl;
+  a[i][n+1]=p;
+ }
+ for(ll i=1;i<=n+1;i++){
+  if(n%2) a[n+1][i]=0;
+  else a[n+1][i]=1;
+ }
+ 
+ 
+ 
+ ll q;
+ cin>>q;
+ 
+ while(q--){
+  ll k;cin>>k;
+  ll x=1,y=1;
+  for(ll i=0;i<2*n-1;i++){
+   cout.flush();
+   cout<<x<<" "<<y<<endl;
+   if(k&(1LL<<(i+1LL))){
+    if(a[x][y+1]) y++;
+    else x++;
+   }
+   else{
+    if(!a[x][y+1]) y++;
+    else x++;
+   }
+  }
+ }
+ 
+ return 0;
+}
