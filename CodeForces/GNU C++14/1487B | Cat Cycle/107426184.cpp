4fdb01b44d5c43df4255ad45443359c1fc973c19
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:48:11 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1487B | Cat Cycle/107426184.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 10000000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+int main()
+{ 
+ 
+ ll t;
+ cin>>t;
+ 
+ while(t--){
+  ll n,k;
+  cin>>n>>k;
+ 
+  ll ans;
+  if(n%2==0){
+   ans=k%n;
+  }
+  else{
+   ll x=n/2;
+   k+=k/x-(k%x==0);
+   ans=k%n;
+  }
+  if(ans==0) ans=n;
+  cout<<ans<<endl;
+ }
+ 
+ return 0;
+}
