abb2131dec24f5d2a110ebbbda650fd27ea1acaa
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 01:29:53 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1579A | Casimir's String Solitaire/130101102.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define Max 100000000000000
+#define min_heap priority_queue <ll, vector<ll>, greater<ll> >
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+int main()
+{
+ 
+    ll t;
+    cin>>t;
+ 
+    while(t--){
+     string s;
+     cin>>s;
+     ll a=count(all(s),'A');
+     ll b=count(all(s),'B');
+     ll c=count(all(s),'C');
+ 
+     if(b==a+c) cout<<"YES"<<endl;
+     else cout<<"NO"<<endl;
+    }
+ 
+    return 0;
+}
