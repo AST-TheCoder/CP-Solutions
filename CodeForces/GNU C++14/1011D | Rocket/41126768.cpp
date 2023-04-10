934b1f23e72834a44b08a1e5c08555eb890bd880
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-04-11 03:04:48 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1011D | Rocket/41126768.cpp
@@ -0,0 +1,101 @@
+/******************************************************/
+/******************************************************/
+/**            BISMILLAHIR RAHMANIR RAHIM            **/
+/**         Al-Shahriar Tonmoy - Red_Sparkle         **/
+/**  Department of Computer Science and Engineering  **/
+/**               Barisal University                 **/
+/**               It's time to bloom.                **/
+/******************************************************/
+/******************************************************/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define     fRead           freopen("input.txt","r",stdin)
+#define     fWrite          freopen("output.txt","w",stdout)
+#define     fast            ios_base::sync_with_stdio(0);cin.tie(0)
+#define     in              scanf
+#define     out             printf
+#define     End             return 0
+#define     ll              long long int
+#define     ul              unsigned long long int
+#define     dob             double
+#define     st              string
+#define     pairl           pair<ll,ll>
+#define     max3(a,b,c)     max(a,max(b,c))
+#define     min3(a,b,c)     min(a,min(b,c))
+#define     fr              first
+#define     se              second
+#define     T               true
+#define     F               false
+#define     nl              "\n"
+#define     all(v)          v.begin(),v.end()
+#define     mem(a,v)        memset(a,v,sizeof(a))
+#define     maxi            1000000000000014
+#define     mini            -100000000000014
+#define     mod             1000000007
+#define     an              1000007
+#define     mapi            map<ll,ll>
+#define     pi              push_back
+#define     po              pop_back
+#define     ve              vector<ll>
+#define     mp              make_pair
+ 
+/*************** #Template FUNCTIONS# **************/
+ 
+ll pc;void PrintCase(){out("Case %lli: ",++pc);}
+ll GCD(ll a,ll b){if(a<b)swap(a,b);if(b==1) return 1;if(!b) return a;if(!(a%b)) return b;else return(a,a%b);}
+ll LCM(ll a,ll b){return (a*b)/GCD(a,b);}
+ll bigmod(ll n,ll p){if(p==0) return 1;if(p==1)return (n+mod)%mod;if(p%2)return (bigmod(n,p-1)*n+mod)%mod;else{ll x=bigmod(n,p/2);return (x*x+mod)%mod;}}
+ll modinverse(ll n){return bigmod(n,mod-2)%mod;}
+bool sc[an];ve pr;void seieve(){for(ll i=2;i<=an;i++){if(!sc[i]){pr.pi(i);for(ll j=i*i;j<=an;j+=i) sc[j]=1;}}}
+pairl pf[an];void pri_fact(ll n){for(ll i=0;i<pr.size() && n!=1;i++){pf[i].fr=pr[i];ll c=0;while(!(n%pr[i])){n/=pr[i];c++;}pf[i].se=c;}}
+ve de;void devisors(ll n){ll i;for(i=1;i<=sqrt(n);i++){if(!(n%i) && i!=sqrt(n)){de.pi(i);de.pi(n%i);}}if((i-1)*(i-1)==n) de.pi(i-1);sort(all(de));}
+ 
+/********************** START **********************/
+ 
+int main()
+{
+ 
+    ll m,n;
+ 
+    cin>>m>>n;
+ 
+    ll tests[n],t,i,qur,first=2,last=m,mid,cycl;
+ 
+    for(i=0;i<n;i++){
+ 
+        cout<<1<<nl;
+ 
+        fflush(stdout);
+ 
+        cin>>t;
+ 
+        if(t==1) tests[i]=1;
+        else tests[i]=0;
+ 
+        if(!t || t==-2) End;
+ 
+    }
+ 
+    for(qur=0;;qur++){
+ 
+        mid=(first+last)/2;
+ 
+        cout<<mid<<nl;
+ 
+        cin>>t;
+ 
+        cycl=qur%n;
+ 
+        if(tests[cycl]==0) t*=-1;
+ 
+        if(t==1) first=mid+1;
+        else last=mid-1;
+ 
+        if(!t || t==-2) End;
+ 
+    }
+ 
+ End;
+}
+/*****************  ALHAMDULILLAH  *****************/
